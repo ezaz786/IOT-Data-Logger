bf67840590bc3399dc01bd004a633a166a603910
ezaz786 <119679077+ezaz786@users.noreply.github.com>
2022-12-10 20:13:16 +0530

    Add files via upload

---
+++ CUT/include/menu.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <struct.h>
+
+extern void menu();
+
+
+#endif
---
+++ CUT/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+#include <menu.h>
+#include <struct.h>
+
+
+int main(int argc, char** argv)
+{	
+	menu();
+	
+	return EXIT_SUCCESS;
+}
